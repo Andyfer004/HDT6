dadd6c315b53c141d9ebcc450cd84e9b6723f4dc
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2023-03-04 23:06:35 -0600

    commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructuras de Datos
+ * Sección 10
+ * 2023
+ * Autores:
+ *      - 22944 Andy Fuentes
+ *      HDT 6
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+}
